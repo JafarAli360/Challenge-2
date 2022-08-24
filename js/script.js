54d4d210bd4393a6295d0d3e3db5fe155c72b4c4
JafarAli360 <ahmad.19058@mhs.unesa.ac.id>
2022-08-25 02:48:05 +0700

    Update Html Css JavaScript

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     margin: 10,
     nav: true,
     autoplay: true,
-    autoplayTimeout: 1000,
+    autoplayTimeout: 2000,
     autoplayHoverPause: true,
     center: true,
     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
